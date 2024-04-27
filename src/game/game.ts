43f064fc32386c4121fc5cbc17939317688dc60e
alex <shizofranky@gmail.com>
2024-04-27 13:59:46 +0300

    refactor

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,4 +1,45 @@
-import { time } from "console";
+import { initializeApp } from "firebase/app";
+
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  setDoc,
+} from "firebase/firestore";
+import { resolve } from "path";
+
+initializeApp({
+  projectId: "test-pro-8c3e9",
+});
+
+const db = getFirestore();
+const colRef = collection(db, "users");
+
+const artificial_delay = async (value: number) => {
+  return new Promise((resolve) => setTimeout(resolve, value, "it s okay"));
+};
+
+const getMyFireDocs = async () => {
+  const response = await artificial_delay(5000);
+  console.log(response);
+
+  const myFireDocs = await getDocs(colRef);
+
+  const arr = myFireDocs.docs.map((elem) => elem.data());
+
+  console.log(arr);
+};
+
+getMyFireDocs();
+
+class ShapeComponent {
+  position: Position;
+  constructor({ x, y }: { x: number; y: number }) {
+    this.position = { x, y };
+  }
+}
 
 interface Position {
   x: number;
@@ -10,10 +51,10 @@ interface Dimensions {
   h: number;
 }
 
-class TetrisElement {
+class Shape {
   isFrozen: boolean;
 
-  components: number[][];
+  components: ShapeComponent[];
 
   position: Position;
   dimensions: Dimensions;
@@ -30,8 +71,8 @@ class TetrisElement {
     for (let i = 0; i < structure.length; i++) {
       ctx.fillStyle = this.isFrozen ? "grey" : "green";
       ctx.fillRect(
-        (structure[i][0] + x) * 25,
-        (structure[i][1] + y) * 25,
+        (structure[i].position.x + x) * 25,
+        (structure[i].position.y + y) * 25,
         w,
         h,
       );
@@ -44,7 +85,7 @@ class TetrisElement {
     w,
     h,
     structure,
-  }: Dimensions & Position & { structure: number[][] }) {
+  }: Dimensions & Position & { structure: ShapeComponent[] }) {
     this.components = structure;
     this.position = { x, y };
     this.dimensions = { w, h };
@@ -53,83 +94,85 @@ class TetrisElement {
 }
 
 export class Game {
-    width:number ;
-    height:number ;
+  width: number;
+  height: number;
   static elemDimensions = 25;
-  allFrozen: TetrisElement[];
-  currentTetrisElement: TetrisElement | null;
+  allFrozenShapes: Shape[];
+  currentAciveShape: Shape | null;
   tetrisScale: number[][];
   lastUpdated = Date.now();
   canvasContext: CanvasRenderingContext2D;
 
   freeze() {
-    const current = this.currentTetrisElement;
+    const current = this.currentAciveShape;
     if (!current) return;
     // alert('inner');
-    this.allFrozen.push(current);
+    this.allFrozenShapes.push(current);
     current.isFrozen = true;
-    this.currentTetrisElement = null;
+    this.currentAciveShape = null;
     // this.currentTetrisElement.
-    console.log(this.currentTetrisElement, this.allFrozen);
+    console.log(this.currentAciveShape, this.allFrozenShapes);
   }
 
   update() {
     const now = Date.now();
 
-    const rows:{y:number , x:number[]}[] = [] ;
+    const rows: { y: number; x: number[] }[] = [];
 
-    this.allFrozen.forEach((elem , i) => {
+    this.allFrozenShapes.forEach((frznShape, i) => {
+      frznShape.components.forEach((component) => {
+        const lev = rows.find(
+          (levelElem) =>
+            levelElem.y === component.position.y + frznShape.position.y,
+        );
 
-        elem.components.forEach(component => {
-            const lev = rows.find(levelElem => levelElem.y === component[1] + elem.position.y);
-
-            if(lev) {
-
-                if(!lev.x.filter(x => elem.position.x + component[0] === x).length) {
-                    lev.x.push(elem.position.x + component[0]);
-                }
-            }
-            else {
-                rows.push({y:component[1] + elem.position.y , x:[component[0] + elem.position.x]})
-            }
-
-        });
-
+        if (lev) {
+          if (
+            !lev.x.filter(
+              (x) => frznShape.position.x + component.position.x === x,
+            ).length
+          ) {
+            lev.x.push(frznShape.position.x + component.position.x);
+          }
+        } else {
+          rows.push({
+            y: component.position.y + frznShape.position.y,
+            x: [component.position.x + frznShape.position.x],
+          });
+        }
+      });
     });
 
-    rows.forEach(row => {
-        if(row.x.length > 5) {
+    rows.forEach((row) => {
+      if (row.x.length > 5) {
+        this.allFrozenShapes.forEach((frozenone) => {
+          const newComponents = frozenone.components.filter((comp) => {
+            return comp.position.y + frozenone.position.y !== row.y;
+          });
 
-            this.allFrozen.forEach(frozenone => {
-                const newComponents = frozenone.components.filter(comp => {
-                    return comp[1] + frozenone.position.y !== row.y
-                });
-
-                frozenone.components = [...newComponents] ;
-            });
-
-        }
+          frozenone.components = [...newComponents];
+        });
+      }
     });
 
     if (now - this.lastUpdated >= 250) {
       this.lastUpdated = now;
 
-      console.log(this.currentTetrisElement, "current");
+      console.log(this.currentAciveShape, "current");
 
-      const current = this.currentTetrisElement;
+      const current = this.currentAciveShape;
 
       if (!current) {
-
-        this.currentTetrisElement = new TetrisElement({
+        this.currentAciveShape = new Shape({
           x: 0,
           y: 0,
           w: 20,
           h: 20,
           structure: [
-            [0, 0],
-            [0, 1],
-            [1, 0],
-            [1, 1],
+            new ShapeComponent({ x: 0, y: 0 }),
+            new ShapeComponent({ x: 1, y: 0 }),
+            new ShapeComponent({ x: 2, y: 0 }),
+            new ShapeComponent({ x: 1, y: 1 }),
           ],
         });
       }
@@ -140,8 +183,8 @@ export class Game {
 
       if (isCollission) {
         current.isFrozen = true;
-        this.allFrozen.push(current);
-        this.currentTetrisElement = null;
+        this.allFrozenShapes.push(current);
+        this.currentAciveShape = null;
       }
 
       if (current && !current.isFrozen) {
@@ -152,10 +195,7 @@ export class Game {
     }
   }
 
-  checkWalls () {
-
-
-  }
+  checkWalls() {}
 
   /**
    *
@@ -167,26 +207,26 @@ export class Game {
   checkCollision(currentElementDelta: { x: number; y: number }) {
     let isCollision = false;
 
-    const current = this.currentTetrisElement;
+    const current = this.currentAciveShape;
 
     if (!current) return false;
 
-    for (const fe of this.allFrozen) {
-      for (const fe_struct_elem of fe.components) {
+    for (const frozenShape of this.allFrozenShapes) {
+      for (const frozenShape_struct_elem of frozenShape.components) {
         if (isCollision) {
           break;
         }
 
-        const feX = fe_struct_elem[0] + fe.position.x;
-        const feY = fe_struct_elem[1] + fe.position.y;
+        const feX = frozenShape_struct_elem.position.x + frozenShape.position.x;
+        const feY = frozenShape_struct_elem.position.y + frozenShape.position.y;
 
         for (const current_struct_elem of current.components) {
           if (isCollision) {
             break;
           }
 
-          const cX = current_struct_elem[0] + current.position.x;
-          const cY = current_struct_elem[1] + current.position.y;
+          const cX = current_struct_elem.position.x + current.position.x;
+          const cY = current_struct_elem.position.y + current.position.y;
 
           if (
             cX + currentElementDelta.x === feX &&
@@ -212,30 +252,28 @@ export class Game {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    const current = this.currentTetrisElement;
+    const current = this.currentAciveShape;
 
     if (current) {
       current.render(this.canvasContext);
     }
 
-    this.allFrozen.forEach((fe) => {
+    this.allFrozenShapes.forEach((fe) => {
       fe.render(ctx);
     });
   }
 
   constructor(ctx: CanvasRenderingContext2D) {
-
-
-    this.width = 20 ;
-    this.height = 40 ;
-    this.currentTetrisElement = null;
+    this.width = 20;
+    this.height = 40;
+    this.currentAciveShape = null;
 
     this.tetrisScale = [...Array(100)].map((elem) => Array(50).fill(0));
 
     this.canvasContext = ctx;
 
     window.addEventListener("keypress", (e) => {
-      const cur = this.currentTetrisElement;
+      const cur = this.currentAciveShape;
 
       switch (e.key) {
         case "w":
@@ -257,8 +295,8 @@ export class Game {
               cur.position.y += delta.y;
             } else {
               cur.isFrozen = true;
-              this.allFrozen.push(cur);
-              this.currentTetrisElement = null;
+              this.allFrozenShapes.push(cur);
+              this.currentAciveShape = null;
             }
           }
           return;
@@ -277,22 +315,20 @@ export class Game {
       }
     });
 
-    // console.log(this.tetrisScale);
-
     const initElement = {
       x: 2,
       y: 0,
       w: 20,
       h: 20,
       structure: [
-        [0, 0],
-        [0, 1],
-        [0, 2],
-        [1, 1],
+        new ShapeComponent({ x: 0, y: 0 }),
+        new ShapeComponent({ x: 1, y: 0 }),
+        new ShapeComponent({ x: 2, y: 0 }),
+        new ShapeComponent({ x: 1, y: 1 }),
       ],
     };
 
-    this.currentTetrisElement = new TetrisElement({ ...initElement });
-    this.allFrozen = [];
+    this.currentAciveShape = new Shape({ ...initElement });
+    this.allFrozenShapes = [];
   }
 }
